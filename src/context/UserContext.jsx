4d1573c9d5d526be562f08314fb432305ac7c0d3
TimoteoGambella <timi.gambella@hotmail.com>
2023-07-16 11:08:23 -0300

    .

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,5 +1,17 @@
 import { createContext, useState } from 'react';
 import { initializeApp } from "firebase/app";
+import {
+    getFirestore,
+    getDocs,
+    query,
+    collection,
+    where,
+    addDoc,
+    doc,
+    setDoc,
+    getDoc,
+    orderBy,
+} from 'firebase/firestore';
 
 export const UseUserContext = createContext();
 
@@ -8,20 +20,36 @@ export const UserContext = ({ children }) => {
     const [user,setUser]=useState(null)
 
     const firebaseConfig = {
-        apiKey: process.env.REACT_APP_FIREBASE,
-        authDomain: "tuciclo-335ab.firebaseapp.com",
-        projectId: "tuciclo-335ab",
-        storageBucket: "tuciclo-335ab.appspot.com",
-        messagingSenderId: "630475593181",
-        appId: "1:630475593181:web:9d633bf2726091f9b6d234"
+      apiKey: process.env.REACT_APP_FIREBASE,
+      authDomain: "dafont-b16fc.firebaseapp.com",
+      projectId: "dafont-b16fc",
+      storageBucket: "dafont-b16fc.appspot.com",
+      messagingSenderId: "475615563360",
+      appId: "1:475615563360:web:dbecc88a95160355b9fd05",
+      measurementId: "G-JWPRWRDGC9"
+    };
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+
+    const addUser = async (newUser) => {
+        const user = await addDoc(collection(db, 'usuarios'), newUser);
+        return user.id;
+        
     };
 
-    const app = initializeApp(firebaseConfig);
-
+    const updateUser = async (idUser, library, balance) => {
+        const user = doc(db, 'usuarios', idUser);
+        // await setDoc(user, { biblioteca: library, mediosPago: balance }, { merge: true });
+        return true;
+    };
+    
     return (
         <UseUserContext.Provider
             value={{
-                user
+                user,
+                addUser
             }}
         >
             {children}
